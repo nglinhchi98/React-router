2cda955d8dd9d13309ef81e5b5b63a2e03fb2080
Chi Nguyen <nglinhchi65@gmail.com>
2023-03-31 00:16:53 +0300

    React router exercises

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h2>This is homepage</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to React Router</h1>
+      <BrowserRouter>
+        <Link to="/" className="link">Home</Link>{''}
+        <Link to="/about" className="link">About</Link>{''}
+        <Link to="/contact" className="link">Contact</Link>{''}
+        <Routes>
+          <Route path="/" element = {<Home />} />
+          <Route path="/about" element = {<About />} />
+          <Route path="/contact" element = {<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
